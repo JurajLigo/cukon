370a97a32efce1cd841f6b3a81e2713a656257bf
Juraj Ligocky <j.ligocky@gmail.com>
2021-10-02 22:35:15 +0200

    add work items: zilina and dumbier

--- src/components/article/article.tsx
+++ src/components/article/article.tsx
@@ -12,15 +12,15 @@ export interface ArticleProps {
 export const Article = (props: ArticleProps) => {
   return (
     <article className="article base-container">
+      <div className="article__text-wrapper">
+        <h2 className="title title--article">{props.title}</h2>
+        <div>{props.content}</div>
+      </div>
       <div className="article__image-wrapper">
         {props.image && <Img fluid={props.image} className="article__image" />}
         {props.videoPath && (
           <iframe className="article__video" src={props.videoPath}></iframe>
         )}
-      </div>
-      <div className="article__text-wrapper">
-        <h2 className="title title--article">{props.title}</h2>
-        <div>{props.content}</div>
       </div>
     </article>
   )
